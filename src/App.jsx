632c73234fbc4d5d4bb468166fe33fa6475fbeef
Aleksandr Burakov <allexburakov@gmail.com>
2023-05-25 01:12:32 +0200

    add some changes on MainPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,5 @@ import './App.css';
 import { MainPage } from './layouts/MainPage';
 
 export function App() {
-  return (
-    <>
-      <MainPage>
-        <p>Login or Signing</p>
-      </MainPage>
-    </>
-  );
+  return <MainPage />;
 }
--- src/layouts/MainPage.jsx
+++ src/layouts/MainPage.jsx
@@ -1,16 +1,21 @@
 import styles from './MainPage.module.css';
+import logo from '../static/logo.svg';
+import { LoginForm } from '../components/LoginForm';
 
 export const MainPage = ({ children }) => {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <main className={styles.main__container}>{children}</main>
-        <div className={styles.presentation}>
-          <p className={styles.presentation__header}>
-            You local vinyl collection
-          </p>
+    <div className={styles.wrapper}>
+      <main className={styles.main__container}>
+        <img className={styles.logo} src={logo} />
+        <div className={styles.container__form}>
+          <LoginForm />
         </div>
+      </main>
+      <div className={styles.presentation}>
+        <p className={styles.presentation__header}>
+          You local vinyl collection
+        </p>
       </div>
-    </>
+    </div>
   );
 };
